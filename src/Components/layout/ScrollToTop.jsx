21047ae33487d2bc36d302e3285fdac797ff1910
Pouriya1384Gholamy <pooriya1384gholamy@gmail.com>
2026-07-06 01:33:47 +0330

    update Fixed Bugs

--- src/Components/layout/ScrollToTop.jsx
+++ src/Components/layout/ScrollToTop.jsx
@@ -8,7 +8,7 @@ const ScrollToTop = () => {
     window.scrollTo({
       top: 0,
       left: 0,
-      behavior: 'instant', // برای جابجایی سریع و بدون انیمیشن به بالای صفحه
+      behavior: 'auto',
     });
   }, [pathname]);
 
