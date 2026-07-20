267b9477e2f44b2289838cd016283e17b2b61fb0
Pouriya1384Gholamy <pooriya1384gholamy@gmail.com>
2026-07-20 19:03:38 +0330

    update Banners Finished

--- src/Pages/Home/Components/Banner.jsx
+++ src/Pages/Home/Components/Banner.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import img from "../../../assets/img/Banner1.png";
 
 function Banner() {
   return (
     <section className="flex flex-col sm:flex-row gap-4 px-4 py-6">
-      <div className="w-full h-[130px] sm:w-1/2 sm:h-[194px] md:h-[235px] h-[140px] bg-gray-200 rounded-xl"></div>
-      <div className="w-full h-[130px] sm:w-1/2 sm:h-[194px] md:h-[235px] h-[140px] bg-gray-200 rounded-xl"></div>
+      <div className="w-full md:w-1/2 h-[160px] sm:h-[234px] md:h-[295px] rounded-xl overflow-hidden flex items-center justify-center">
+        <img 
+          src={img} 
+          alt="Banner Of Eriss Wood"
+          className="w-full h-full object-scale-down"
+        />
+      </div>
+      <div className="w-full md:w-1/2 h-[160px] sm:h-[234px] md:h-[295px] rounded-xl overflow-hidden flex items-center justify-center">
+        <img 
+          src={img} 
+          alt="Banner Of Eriss Wood"
+          className="w-full h-full object-scale-down"
+        />
+      </div>
     </section>
   );
 }
